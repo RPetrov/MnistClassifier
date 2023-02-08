89b801b5eee10815fe0c5bcff04f0ffc4e6293ff
rpetrov <roman.al.petrov@yandex.ru>
2023-02-08 12:19:43 +0300

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main(args: Array<String>) {
+
+    val linesFromFile = Files.readAllLines(Paths.get("archive_mnist\\mnist_test.csv"))
+
+    println(linesFromFile.size)
+}
