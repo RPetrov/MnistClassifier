320b9e4b22fab4f9c6330c7af54c6a5873b07aec
ra.petrov <ra.petrov@tensor.ru>
2023-02-14 18:35:26 +0300

    update path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 fun main(args: Array<String>) {
 
-    val linesFromFile = Files.readAllLines(Paths.get("archive_mnist\\mnist_test.csv"))
+    val linesFromFile = Files.readAllLines(Paths.get("archive_mnist\\mnist_train.csv"))
 
     println(linesFromFile.size)
 }
