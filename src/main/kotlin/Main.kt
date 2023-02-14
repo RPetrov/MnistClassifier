aca8e533199a2614c7ed8a2d543fe94b6e72a3fb
Yan <yantsabut777@yandex.ru>
2023-02-14 19:44:46 +0300

    добавлен class Image

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,22 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 
+data class Image(val value: Int, val data: IntArray) {
+    companion object {
+        fun parse(line: String): Image {
+            // 0,2,5...
+            val number = line.split(",")
+            val value = number[0].toInt()
+            val data = number.subList(1, number.size).map { it.toInt() }.toIntArray()
+            return Image(value, data)
+        }
+    }
+}
+
 fun main(args: Array<String>) {
 
     val linesFromFile = Files.readAllLines(Paths.get("archive_mnist\\mnist_test.csv"))
-
+    val images = linesFromFile.map { Image.parse(it) }
     println(linesFromFile.size)
     println("Hello")
 }
