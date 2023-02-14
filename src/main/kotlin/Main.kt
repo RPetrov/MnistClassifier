1031231be86d800edb8733f0ea4c74eda67b1952
olga <yep4yep@gmail.com>
2023-02-15 01:16:00 +0800

    added visialization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 data class Image(val value: Int, val data: IntArray) {
+
     companion object {
         fun parse(line: String): Image {
             // 0,2,5...
@@ -11,12 +12,24 @@ data class Image(val value: Int, val data: IntArray) {
             return Image(value, data)
         }
     }
+
+    override fun toString(): String {
+        val stringbuilder = StringBuilder()
+        for (i in 0 until 28) {
+            for (j in 0 until 28) {
+                stringbuilder.append(if (data[i * 28 + j] > 127) "*" else " ")
+            }
+            stringbuilder.append("\n")
+        }
+        return stringbuilder.toString()
+    }
 }
 
 fun main(args: Array<String>) {
 
     val linesFromFile = Files.readAllLines(Paths.get("archive_mnist\\mnist_train.csv"))
-    val images = linesFromFile.map { Image.parse(it) }
+    val images = linesFromFile.subList(1, linesFromFile.size).map { Image.parse(it) }
+    images.take(10).forEach { print(it) }
     println(linesFromFile.size)
     println("Hello")
 }
